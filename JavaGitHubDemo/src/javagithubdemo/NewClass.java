fb67fe56f13e5a68d354f2ad93b96ea3ed8fc6ba
Ken Rylander <ken@lajbans.com.au>
2020-09-04 17:27:58 +0200

    Added getters setters

---
+++ JavaGitHubDemo/src/javagithubdemo/NewClass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagithubdemo;
+
+/**
+ *
+ * @author kenrylander
+ */
+public class NewClass {
+
+    private int x1;
+    private String sdjh;
+
+    public int getX1() {
+        return x1;
+    }
+
+    /**
+     * @param x1 the x1 to set
+     */
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    /**
+     * @return the sdjh
+     */
+    public String getSdjh() {
+        return sdjh;
+    }
+
+    /**
+     * @param sdjh the sdjh to set
+     */
+    public void setSdjh(String sdjh) {
+        this.sdjh = sdjh;
+    }
+    
+    
+}
